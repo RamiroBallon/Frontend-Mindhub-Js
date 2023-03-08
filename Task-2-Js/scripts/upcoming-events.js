198d74ac9d809ea01f5519cf34f0e5180539a18c
RamiroBallon <ramiroballonrios@gmail.com>
2023-03-08 16:13:55 -0300

    :new: subo task 2 - tarjetas dinamicas Js

---
+++ Task-2-Js/scripts/upcoming-events.js
@@ -0,0 +1,35 @@
+console.log("Script is working")
+const contenedorElementos=document.querySelector("#contenedor-cards") /*getElementById(contenedor-cards)*/ 
+const events=data.events
+const currentDate=data.currentDate
+
+
+console.log(contenedorElementos)
+console.log(events)
+console.log(currentDate)
+
+mostrarTarjetas(events, contenedorElementos)
+
+function mostrarTarjetas(arrayDatos, contenedor) {
+    let tarjetas = ""
+    for (let event of arrayDatos) {
+        if (event.date > currentDate) {
+            tarjetas += `<div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img src="${event.image}" class="card-img-top img-fluid" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${event.name}</h5>
+                    <p class="card-text">${event.description}</p>
+                    <div class="info-adicional">
+                    <p>Date: ${event.date}</p>
+                    <p>Place: ${event.place}</p>
+                </div>
+                    <p class="precio">Price: $${event.price}</p>
+                    <a href="details.html" class="btn btn-primary">More info</a>
+                </div>
+            </div>
+        </div>`
+        }
+    }
+    contenedor.innerHTML=tarjetas
+}
